83239910df750dff077ebb490a0202e3c736326f
vladislav <vladdbaf@mail.ru>
2023-07-13 15:23:08 +0300

    first commit

---
+++ src/main/java/com/example/secondwebfluxexample/Role.java
@@ -0,0 +1,6 @@
+package com.example.secondwebfluxexample;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/example/secondwebfluxexample/User.java
@@ -0,0 +1,26 @@
+package com.example.secondwebfluxexample;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private Boolean enabled;
+
+    private Role role;
+}
